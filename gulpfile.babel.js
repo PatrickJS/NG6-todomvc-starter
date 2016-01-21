a79e3ffde04808b5fceefc6809f675bc3a8a52ef
Sergey Protko <fesors@gmail.com>
2016-01-21 17:36:14 +0300

    Update buildstack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,47 +4,41 @@ import gulp     from 'gulp';
 import webpack  from 'webpack';
 import path     from 'path';
 import sync     from 'run-sequence';
-import serve    from 'browser-sync';
-import rename   from 'gulp-rename';
 import fs       from 'fs';
 import yargs    from 'yargs';
-import gutil    from 'gulp-util'
+import gutil    from 'gulp-util';
+import serve    from 'browser-sync';
+import webpackDevMiddelware from 'webpack-dev-middleware';
+import webpachHotMiddelware from 'webpack-hot-middleware';
+import colorsSupported      from 'supports-color';
 
-let reload = () => serve.reload();
-const
-  root = 'src',
-  output = 'dist';
+let root = 'src';
 
 // helper method for resolving paths
-function resolveToApp (glob = '') {
-
+let resolveToApp = (glob = '') => {
   return path.join(root, 'app', glob); // app/{glob}
-}
+};
 
-function resolveToComponents(glob = '') {
-
+let resolveToComponents = (glob = '') => {
   return path.join(root, 'app/components', glob); // app/components/{glob}
-}
+};
 
 // map of all paths
 let paths = {
-  js: [
-    resolveToApp('**/*!(.spec.js).js'),
-    resolveToComponents('**/*!(.spec.js).js')
-  ], // exclude spec files
-  less: resolveToApp('**/*.less'), // stylesheets
+  js: resolveToComponents('**/*!(.spec.js).js'), // exclude spec files
+  styl: resolveToApp('**/*.styl'), // stylesheets
   html: [
-    resolveToApp('**/*.html')
+    resolveToApp('**/*.html'),
+    path.join(root, 'index.html')
   ],
-  index: path.join(root, 'index.html'),
-  entry: './' + path.join(root, 'app/app.js'),
-  output: output,
+  entry: path.join(__dirname, root, 'app/app.js'),
+  output: root,
   blankTemplates: path.join(__dirname, 'generator', 'component/**/*.**')
 };
 
 // use webpack.config.js to build modules
 gulp.task('webpack', (cb) => {
-  var config = require('./webpack.config');
+  const config = require('./webpack.dist.config');
   config.entry.app = paths.entry;
 
   webpack(config, (err, stats) => {
@@ -53,31 +47,45 @@ gulp.task('webpack', (cb) => {
     }
 
     gutil.log("[webpack]", stats.toString({
-      timings: true,
-      chunks: false
+      colors: colorsSupported,
+      chunks: false,
+      errorDetails: true
     }));
+
     cb();
   });
 });
 
-gulp.task('index', ['webpack'], () => {
-  return gulp.src(paths.index)
-    .pipe(gulp.dest(paths.output))
-});
-
 gulp.task('serve', () => {
+  const config = require('./webpack.dev.config');
+  config.entry.app = [
+    // this modules required to make HRM working
+    // it responsible for all this webpack magic
+    'webpack-hot-middleware/client?reload=true',
+    // application entry point
+    paths.entry
+  ];
+
+  var compiler = webpack(config);
+
   serve({
     port: process.env.PORT || 3000,
     open: false,
-    server: { baseDir: output }
+    server: {baseDir: root},
+    middleware: [
+      webpackDevMiddelware(compiler, {
+        stats: {
+          colors: colorsSupported,
+          chunks: false,
+          modules: false
+        },
+        publicPath: config.output.publicPath
+      }),
+      webpachHotMiddelware(compiler)
+    ]
   });
 });
 
-gulp.task('watch', () => {
-  let allPaths = [].concat([paths.js], paths.html, [paths.index], [paths.less]);
-  gulp.watch(allPaths, ['webpack', reload]);
-});
+gulp.task('watch', ['serve']);
 
-gulp.task('default', (done) => {
-  sync('index', 'serve', 'watch', done);
-});
+gulp.task('default', ['serve']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
-var path              = require('path');
-var webpack           = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path    = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'sourcemap',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  entry: {
-    vendors: ['angular']
-  },
+  entry: {},
   module: {
     loaders: [
        { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.less$/, loader: 'style!css!less' },
-       { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
+       { test: /\.css$/, loader: 'style!css' }
     ]
   },
   plugins: [
-    new ExtractTextPlugin("styles.css"),
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    // Automatically move all modules defined outside of application directory to vendor bundle.
+    // If you are using more complicated project structure, consider to specify common chunks manually.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
+      }
+    })
   ]
 };
