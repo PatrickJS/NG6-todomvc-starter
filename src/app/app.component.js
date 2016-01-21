9761ca1347184c951edc01e24cadccf49021f0af
Sergey Protko <fesors@gmail.com>
2016-01-21 17:53:48 +0300

    Update angular-component

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -42,16 +42,16 @@ export default {
         <header class="header-input">
           <todo-text-input
             placeholder="What needs to get done?"
-            on-save="app.onSave(task)">
+            on-save="$ctrl.onSave(task)">
           </todo-text-input>
         </header>
       </section>
       <section class="main">
-        <todo-batch-toggle on-toggle="app.onToggleAll()" todos="app.todos.list" ng-if="app.todos.list.length"></todo-batch-toggle>
-        <todo-list todos="app.todos.filteredList"></todo-list>
+        <todo-batch-toggle on-toggle="$ctrl.onToggleAll()" todos="$ctrl.todos.list" ng-if="$ctrl.todos.list.length"></todo-batch-toggle>
+        <todo-list todos="$ctrl.todos.filteredList"></todo-list>
       </section>
-      <todo-footer ng-if="app.todos.list.length" todos="app.todos">
-        <todo-list-filter on-filter="app.onFilter(state)" filter-state="app.todos.filterState"></todo-list-filter>
+      <todo-footer ng-if="$ctrl.todos.list.length" todos="$ctrl.todos">
+        <todo-list-filter on-filter="$ctrl.onFilter(state)" filter-state="$ctrl.todos.filterState"></todo-list-filter>
       </todo-footer>
     </section>
   `,
